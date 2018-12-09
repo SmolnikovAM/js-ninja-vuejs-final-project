b2720789e2e4bdb1b0d98d90d5ccb055bb5bd7ab
Andrei Smolnikov <andr.sml@gmail.com>
2018-12-09 20:06:33 +0200

    TimeInterval structure

--- frontend/src/components/TimeInterval/index.vue
+++ frontend/src/components/TimeInterval/index.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>TimeInterval</div>
+  <div>
+    <check-box/>
+    <day-of-week/>
+    <time-picker/>
+    <span>to</span>
+    <time-picker/>
+    <button>edit</button>
+  </div>
 </template>
 
 <script>
+import CheckBox from '../CheckBox';
+import DayOfWeek from '../DayOfWeek';
+import TimePicker from '../TimePicker';
+
 export default {
   name: 'TimeInterval',
+  components: { CheckBox, DayOfWeek, TimePicker },
 };
 </script>
 
