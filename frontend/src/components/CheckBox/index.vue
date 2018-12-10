52f9e2e2afc0e7efbf4e1028519b893c87bd5ab1
Andrei Smolnikov <andr.sml@gmail.com>
2018-12-10 17:05:42 +0200

    CheckBox model

--- frontend/src/components/CheckBox/index.vue
+++ frontend/src/components/CheckBox/index.vue
@@ -1,6 +1,6 @@
 <template>
   <label class="label-wrapper">
-    <input type="checkbox" class="input">
+    <input type="checkbox" class="input" :checked="checked" @change="$emit('change', !checked)">
     <span class="box">
       <span class="mark"></span>
     </span>
@@ -9,8 +9,25 @@
 </template>
 
 <script>
+const EVENT = 'change';
+
 export default {
   name: 'CheckBox',
+  model: {
+    prop: 'checked',
+    event: EVENT,
+  },
+  props: {
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    change() {
+      this.$emit(EVENT, !this.checked);
+    },
+  },
 };
 </script>
 
