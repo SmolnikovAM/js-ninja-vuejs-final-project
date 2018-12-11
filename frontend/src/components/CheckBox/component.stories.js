1516fa96e1110dcdda8388f9b371175f17a5d4a9
Andrei Smolnikov <andr.sml@gmail.com>
2018-12-11 18:25:27 +0200

    reserved word bugfix

--- frontend/src/components/CheckBox/component.stories.js
+++ frontend/src/components/CheckBox/component.stories.js
@@ -1,41 +1,36 @@
 import { storiesOf } from '@storybook/vue';
 
-import Component from '.';
+import MyComponent from '.';
 
-storiesOf(Component.name, module)
-  .add(`display`, () => ({
-    components: { Component },
-    template: `<div><component/></div>`,
-  }))
-  .add(`states`, () => ({
-    components: { Component },
-    data: () => ({
-      checkBox01: true,
-      checkBox02: false,
-      checkBox03: true,
-      checkBox04: true,
-    }),
-    mounted() {
-      this.$refs.first.$el.focus();
-    },
-    template: `
+storiesOf(MyComponent.name, module).add(`display`, () => ({
+  components: { MyComponent },
+  data: () => ({
+    checkBox01: true,
+    checkBox02: false,
+    checkBox03: true,
+    checkBox04: true,
+  }),
+  mounted() {
+    this.$refs.first.$el.focus();
+  },
+  template: `
   <div>
      <h4>checkBox01 {{ checkBox01 }}</h4>
-     <component v-model="checkBox01"  ref="first"/>
+     <my-component v-model="checkBox01"  ref="first"/>
      <h4>checkBox02 {{ checkBox02 }}</h4>
-     <component v-model="checkBox02"/>
+     <my-component v-model="checkBox02"/>
      
      <h4>checkBox03 {{ checkBox03 }}</h4>
-     <component v-model="checkBox03">
+     <my-component v-model="checkBox03">
       Slot text.
-     </component> 
+     </my-component> 
      <span style="color:green">
       After text.
      </span>
 
      <h4>checkBox04 {{ checkBox04 }}</h4>
-     <component v-model="checkBox04" tabindex="-1">
+     <my-component v-model="checkBox04" tabindex="-1">
       No focus
-     </component> 
+     </my-component> 
   </div>`,
-  }));
+}));
--- frontend/src/components/TimePicker/component.stories.js
+++ frontend/src/components/TimePicker/component.stories.js
@@ -1,14 +1,11 @@
 import { storiesOf } from '@storybook/vue';
 
-import Component from '.';
+import MyComponent from '.';
 
-storiesOf(Component.name, module).add(`display "${Component.name}"`, () => ({
-  components: { Component },
-  template: `<div><component/></div>`,
-}));
-// .add('additional story',() => ({
-//   components: { [Component.name]: Component },
-//   data: () => ({}),
-//   methods: {},
-//   template: `<div><check-box/></div>`,
-// }));
+storiesOf(MyComponent.name, module).add(
+  `display "${MyComponent.name}"`,
+  () => ({
+    components: { MyComponent },
+    template: `<div><my-component/></div>`,
+  }),
+);
