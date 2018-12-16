311819e0fe21bdde9d28e95a67b3277f13fe92ab
Andrei Smolnikov <andr.sml@gmail.com>
2018-12-16 16:17:43 +0200

    CheckBox test prop chcked true

--- frontend/src/components/CheckBox/component.spec.js
+++ frontend/src/components/CheckBox/component.spec.js
@@ -1,5 +1,12 @@
-describe('test jest is working', () => {
-  test('2+2=4', () => {
-    expect(2 + 2).toBe(4);
+import { mount } from '@vue/test-utils';
+import CheckBox from '.';
+
+describe(`Component ${CheckBox.name}`, () => {
+  test('prop checked', () => {
+    const wrapper = mount(CheckBox, {
+      propsData: { checked: true },
+    });
+    const mark = wrapper.find('.mark-position');
+    expect(mark.exists()).toBe(true);
   });
 });
