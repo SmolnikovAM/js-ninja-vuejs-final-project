ec6779d51fae5d848491017258c1ac4ee3c9dece
Andrei Smolnikov <andr.sml@gmail.com>
2018-12-17 14:52:34 +0200

    CheckBox list of tests

--- frontend/src/components/CheckBox/component.spec.js
+++ frontend/src/components/CheckBox/component.spec.js
@@ -2,11 +2,21 @@ import { mount } from '@vue/test-utils';
 import CheckBox from '.';
 
 describe(`Component ${CheckBox.name}`, () => {
-  test('prop checked', () => {
+  test('Prop checked true', () => {
     const wrapper = mount(CheckBox, {
       propsData: { checked: true },
     });
     const mark = wrapper.find('.mark-position');
     expect(mark.exists()).toBe(true);
   });
+
+  test.skip('Prop checked false', () => {});
+  test.skip('Change prop checked', () => {});
+  test.skip('Slot text render', () => {});
+  test.skip('Click on checkbox(label). Prop checked change', () => {});
+  test.skip('Click on checkbox(span). Prop checked change', () => {});
+  test.skip('Click on disabled checkbox(label). Prop checked not change', () => {});
+  test.skip('Disabled checkbox has special classes', () => {});
+  test.skip('Autofocus on start', () => {});
+  test.skip('Focus by Tab', () => {});
 });
