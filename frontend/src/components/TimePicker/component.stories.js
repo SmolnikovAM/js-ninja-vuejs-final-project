8d6370e2cfe24afe2e2341bcc87b82665b97b67c
SmolnikovAM <andr.sml@gmail.com>
2019-01-22 17:29:00 +0300

    TimePicker stories

--- frontend/src/components/TimePicker/component.stories.js
+++ frontend/src/components/TimePicker/component.stories.js
@@ -6,6 +6,16 @@ storiesOf(MyComponent.name, module).add(
   `display "${MyComponent.name}"`,
   () => ({
     components: { MyComponent },
-    template: `<div><my-component/></div>`,
+    data: () => ({
+      date: new Date(2018, 11, 25, 13, 2), // 13:02
+      style: 'padding: 10px; margin: 10px; border: 1px solid black;',
+    }),
+    template: `
+    <div>
+      <div :style="style">
+        show time "new Date(2018, 11, 25, 13, 2)": <br><br>
+         <my-component :date="date"/>
+      </div>      
+    </div>`,
   }),
 );
