4fa8c56812fb796585f648158462060e6ed08e45
SmolnikovAM <andr.sml@gmail.com>
2019-01-22 17:12:07 +0300

    TimePicker component

--- frontend/src/components/TimePicker/index.vue
+++ frontend/src/components/TimePicker/index.vue
@@ -1,12 +1,36 @@
 <template>
-  <span>TimePicker</span>
+  <span>
+    <span>{{ dateToString }}</span>
+    <span>{{ dateAMPM }}</span>
+  </span>
 </template>
 
 <script>
+const addZeros = d => `00${d ? d : ''}`.slice(-2);
+
 export default {
   name: 'TimePicker',
+  props: {
+    date: { type: Date, default: null },
+  },
+  computed: {
+    dateToString() {
+      if (this.date) {
+        const hh = this.date.getHours();
+        const min = addZeros(this.date.getMinutes());
+        return `${hh % 12 || 12}:${min}`;
+      }
+      return '';
+    },
+
+    dateAMPM() {
+      if (this.date) {
+        const hh = this.date.getHours();
+        return hh > 12 ? 'PM' : 'AM';
+      }
+      return '';
+    },
+  },
 };
 </script>
 
-<style scoped>
-</style>
